996f70ad2b60f040a5407b022e0428f87fb01d08
Daniel Schuerhoff <daniel.schuerhoff@rwth-aachen.de>
2016-08-10 14:28:58 +0200

    Add ThreadedPlayPause to simulation.py for easier use of threading

--- ui-python/core/simulation.py
+++ ui-python/core/simulation.py
@@ -1,5 +1,6 @@
 import corelib
 import ctypes
+import threading
 
 ### Load Library
 _core = corelib.LoadCoreLibrary()
@@ -12,6 +13,13 @@ _PlayPause.restype  = None
 def PlayPause(p_state, method_type, optimizer_type, n_iterations=-1, log_steps=-1, idx_image=-1, idx_chain=-1):
     _PlayPause(p_state, ctypes.c_char_p(method_type), ctypes.c_char_p(optimizer_type), ctypes.c_int(n_iterations), ctypes.c_int(log_steps), ctypes.c_int(idx_image), ctypes.c_int(idx_chain))
 
+### Setup State
+def ThreadedPlayPause(p_state, method_type, optimizer_type, n_iterations=-1, log_steps=-1, idx_image=-1, idx_chain=-1):
+    t = threading.Thread(target=PlayPause, args=[p_state, b"LLG", b"SIB", n_iterations, log_steps, idx_image, idx_chain])
+    t.daemon = True
+    t.start()
+    while t.is_alive(): # wait for the thread to exit
+        t.join(.1)
 
 ### Stop All
 _Stop_All           = _core.Simulation_Stop_All
