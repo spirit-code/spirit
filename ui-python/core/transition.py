c2858132a89dd1dbfdf9972daf83c40069522097
Gideon <g.mueller@fz-juelich.de>
2016-08-25 15:07:19 +0200

    UI-Python: wrote parameters getters. Fixed wrong Set_GNEB_Climbing_Falling.

--- ui-python/core/transition.py
+++ ui-python/core/transition.py
@@ -13,7 +13,6 @@ def Homogeneous(p_state, idx_1, idx_2, idx_chain=-1):
     _Homogeneous(p_state, idx_1, idx_2, idx_chain)
 
 ### Add some temperature-scaled noise to a transition between two images of a chain
-# Transition_Add_Noise_Temperature
 _Add_Noise_Temperature             = _core.Transition_Add_Noise_Temperature
 _Add_Noise_Temperature.argtypes    = [ctypes.c_void_p, ctypes.c_double, ctypes.c_int, ctypes.c_int, ctypes.c_int]
 _Add_Noise_Temperature.restype     = None
