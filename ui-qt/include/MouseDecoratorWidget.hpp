08d35144d7427c8ae3ba7f15ccbc19e8014599f9
Gideon <g.mueller@fz-juelich.de>
2017-02-27 23:44:41 +0100

    UI-QT: added rotation to mouse decorator.

--- ui-qt/include/MouseDecoratorWidget.hpp
+++ ui-qt/include/MouseDecoratorWidget.hpp
@@ -3,11 +3,20 @@
 #define MOUSE_DECORATOR_WIDGET_H
 
 #include <QWidget>
+#include <QTimer>
 
 class MouseDecoratorWidget : public QWidget
 {
 public:
+	MouseDecoratorWidget();
 	void MouseDecoratorWidget::paintEvent(QPaintEvent *);
+private slots:
+	void incrementRotation();
+private:
+	QTimer * m_rotation_timer;
+	int m_rotation_angle;
+	int m_rpm;
+	int m_rotation_updates_per_second;
 };
 
 #endif
--- ui-qt/src/MouseDecoratorWidget.cpp
+++ ui-qt/src/MouseDecoratorWidget.cpp
@@ -2,6 +2,17 @@
 
 #include <QPainter>
 
+MouseDecoratorWidget::MouseDecoratorWidget()
+{
+	this->m_rotation_angle = 0;
+	this->m_rotation_updates_per_second = 40;
+	this->m_rpm = 30;
+
+	this->m_rotation_timer = new QTimer(this);
+	connect(this->m_rotation_timer, &QTimer::timeout, this, &MouseDecoratorWidget::incrementRotation);
+	this->m_rotation_timer->start(std::max(1,1000/this->m_rotation_updates_per_second));
+}
+
 void MouseDecoratorWidget::paintEvent(QPaintEvent *)
 {
 	float radius = 80;
@@ -24,5 +35,12 @@ void MouseDecoratorWidget::paintEvent(QPaintEvent *)
 	pen = QPen(Qt::blue, 3, Qt::PenStyle::CustomDashLine);
 	pen.setDashPattern({ 25, 25 });
 	painter.setPen(pen);
-	painter.drawArc(shift, shift, 2 * (radius - shift), 2 * (radius - shift), 0, 16 * 360);
+	painter.drawArc(shift, shift, 2 * (radius - shift), 2 * (radius - shift), 16 * this->m_rotation_angle, 16 * 360);
+}
+
+void MouseDecoratorWidget::incrementRotation()
+{
+	int add_angle =  6 * m_rpm / m_rotation_updates_per_second;
+	this->m_rotation_angle = (m_rotation_angle + add_angle) % 360;
+	this->update();
 }
