92708c523afb281c4654e57468256bc3c587c2d4
Gideon <g.mueller@fz-juelich.de>
2017-04-28 23:15:58 +0200

    Core: filled out Parameters API stubs.

--- core/src/Spirit/Parameters.cpp
+++ core/src/Spirit/Parameters.cpp
@@ -159,27 +159,67 @@ void Parameters_Set_LLG_STT(State *state, float magnitude, const float * normal,
 // Set MC Output
 void Parameters_Set_MC_Output_Folder(State *state, const char * folder, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+	image->Lock();
+    image->mc_parameters->output_folder = folder;
+	image->Unlock();
+
+	Log(Utility::Log_Level::Info, Utility::Log_Sender::API,
+        "Set MC Output Folder = " + std::string(folder), idx_image, idx_chain);
 }
 
 void Parameters_Set_MC_Output_General(State *state, bool any, bool initial, bool final, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+	image->Lock();
+    image->mc_parameters->output_any = any;
+    image->mc_parameters->output_initial = initial;
+    image->mc_parameters->output_final = final;
+	image->Unlock();
 }
 
 void Parameters_Set_MC_Output_Energy(State *state, bool energy_step, bool energy_archive, bool energy_spin_resolved, bool energy_divide_by_nos, int idx_image, int idx_chain)
 {
-
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
+    
+	image->Lock();
+    image->mc_parameters->output_energy_step = energy_step;
+    image->mc_parameters->output_energy_archive = energy_archive;
+    image->mc_parameters->output_energy_spin_resolved = energy_spin_resolved;
+    image->mc_parameters->output_energy_divide_by_nspins = energy_divide_by_nos;
+	image->Unlock();
 }
 
 void Parameters_Set_MC_Output_Configuration(State *state, bool configuration_step, bool configuration_archive, int idx_image, int idx_chain)
 {
-
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
+    
+	image->Lock();
+    image->mc_parameters->output_configuration_step = configuration_step;
+    image->mc_parameters->output_configuration_archive = configuration_archive;
+	image->Unlock();
 }
 
 void Parameters_Set_MC_N_Iterations(State *state, int n_iterations, int n_iterations_log, int idx_image, int idx_chain)
 {
-
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
+    
+	image->Lock();
+    image->mc_parameters->n_iterations = n_iterations;
+    image->mc_parameters->n_iterations_log = n_iterations_log;
+	image->Unlock();
 }
 
 
@@ -223,22 +263,54 @@ void Parameters_Set_MC_Acceptance_Ratio(State *state, float ratio, int idx_image
 // Set GNEB Output
 void Parameters_Set_GNEB_Output_Folder(State *state, const char * folder, int idx_chain)
 {
+	int idx_image = -1;
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+	chain->Lock();
+    chain->gneb_parameters->output_folder = folder;
+	chain->Unlock();
 }
 
 void Parameters_Set_GNEB_Output_General(State *state, bool any, bool initial, bool final, int idx_chain)
 {
+	int idx_image = -1;
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+	chain->Lock();
+    chain->gneb_parameters->output_any = any;
+    chain->gneb_parameters->output_initial = initial;
+    chain->gneb_parameters->output_final = final;
+	chain->Unlock();
 }
 
 void Parameters_Set_GNEB_Output_Energies(State *state, bool energies_step, bool energies_interpolated, bool energies_divide_by_nos, int idx_chain)
 {
+	int idx_image = -1;
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+	chain->Lock();
+    chain->gneb_parameters->output_energies_step = energies_step;
+    chain->gneb_parameters->output_energies_interpolated = energies_interpolated;
+    chain->gneb_parameters->output_energies_divide_by_nspins = energies_divide_by_nos;
+	chain->Unlock();
 }
 
 void Parameters_Set_GNEB_Output_Chain(State *state, bool chain_step, int idx_chain)
 {
+	int idx_image = -1;
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+	chain->Lock();
+    chain->gneb_parameters->output_chain_step = chain_step;
+	chain->Unlock();
 }
 
 void Parameters_Set_GNEB_N_Iterations(State *state, int n_iterations, int n_iterations_log, int idx_chain)
@@ -301,17 +373,35 @@ const char * Parameters_Get_LLG_Output_Folder(State *state, int idx_image, int i
 
 void Parameters_Get_LLG_Output_General(State *state, bool * any, bool * initial, bool * final, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *any = image->llg_parameters->output_any;
+    *initial = image->llg_parameters->output_initial;
+    *final = image->llg_parameters->output_final;
 }
 
 void Parameters_Get_LLG_Output_Energy(State *state, bool * energy_step, bool * energy_archive, bool * energy_spin_resolved, bool * energy_divide_by_nos, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *energy_step = image->llg_parameters->output_energy_step;
+    *energy_archive = image->llg_parameters->output_energy_archive;
+    *energy_spin_resolved = image->llg_parameters->output_energy_spin_resolved;
+    *energy_divide_by_nos = image->llg_parameters->output_energy_divide_by_nspins;
 }
 
 void Parameters_Get_LLG_Output_Configuration(State *state, bool * configuration_step, bool * configuration_archive, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *configuration_step = image->llg_parameters->output_configuration_step;
+    *configuration_archive = image->llg_parameters->output_configuration_archive;
 }
 
 void Parameters_Get_LLG_N_Iterations(State *state, int * iterations, int * iterations_log, int idx_image, int idx_chain)
@@ -387,22 +477,46 @@ const char * Parameters_Get_MC_Output_Folder(State *state, int idx_image, int id
 
 void Parameters_Get_MC_Output_General(State *state, bool * any, bool * initial, bool * final, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *any = image->mc_parameters->output_any;
+    *initial = image->mc_parameters->output_initial;
+    *final = image->mc_parameters->output_final;
 }
 
 void Parameters_Get_MC_Output_Energy(State *state, bool * energy_step, bool * energy_archive, bool * energy_spin_resolved, bool * energy_divide_by_nos, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *energy_step = image->mc_parameters->output_energy_step;
+    *energy_archive = image->mc_parameters->output_energy_archive;
+    *energy_spin_resolved = image->mc_parameters->output_energy_spin_resolved;
+    *energy_divide_by_nos = image->mc_parameters->output_energy_divide_by_nspins;
 }
 
 void Parameters_Get_MC_Output_Configuration(State *state, bool * configuration_step, bool * configuration_archive, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *configuration_step = image->mc_parameters->output_configuration_step;
+    *configuration_archive = image->mc_parameters->output_configuration_archive;
 }
 
 void Parameters_Get_MC_N_Iterations(State *state, int * iterations, int * iterations_log, int idx_image, int idx_chain)
 {
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    auto p = image->mc_parameters;
+    *iterations = p->n_iterations;
+    *iterations_log = p->n_iterations_log;
 }
 
 // Get MC Simulation Parameters
@@ -442,17 +556,36 @@ const char * Parameters_Get_GNEB_Output_Folder(State *state, int idx_chain)
 
 void Parameters_Get_GNEB_Output_General(State *state, bool * any, bool * initial, bool * final, int idx_chain)
 {
+	int idx_image = -1;
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *any = chain->gneb_parameters->output_any;
+    *initial = chain->gneb_parameters->output_initial;
+    *final = chain->gneb_parameters->output_final;
 }
 
 void Parameters_Get_GNEB_Output_Energies(State *state, bool * energies_step, bool * energies_interpolated, bool * energies_divide_by_nos, int idx_chain)
 {
+	int idx_image = -1;
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *energies_step = chain->gneb_parameters->output_energies_step;
+    *energies_interpolated = chain->gneb_parameters->output_energies_interpolated;
+    *energies_divide_by_nos = chain->gneb_parameters->output_energies_divide_by_nspins;
 }
 
 void Parameters_Get_GNEB_Output_Chain(State *state, bool * chain_step, bool *, int idx_chain)
 {
+	int idx_image = -1;
+    std::shared_ptr<Data::Spin_System> image;
+    std::shared_ptr<Data::Spin_System_Chain> chain;
+    from_indices(state, idx_image, idx_chain, image, chain);
 
+    *chain_step = chain->gneb_parameters->output_chain_step;
 }
 
 void Parameters_Get_GNEB_N_Iterations(State *state, int * iterations, int * iterations_log, int idx_chain)
