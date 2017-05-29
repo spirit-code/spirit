9bda85ec22357ba8ef37ec02d2fcfb9778d4f4b4
Gideon <g.mueller@fz-juelich.de>
2017-05-29 17:51:08 +0200

    Updated PathsIFF for new gcc6.

--- CMake/PathsIFF.cmake
+++ CMake/PathsIFF.cmake
@@ -4,7 +4,7 @@ if( USER_PATHS_IFF )
     ### GCC Compiler
     set( USER_COMPILER_C    "gcc" )
     set( USER_COMPILER_CXX  "g++" )
-    set( USER_PATH_COMPILER "/usr/local/gcc/bin" )
+    set( USER_PATH_COMPILER "/usr/local/gcc6/bin" )
     ### QT Location
     set( USER_PATH_QT       "/usr/local/qt5" )
 endif()
