32d3dd78c46caaeaef960e243babb2ef0e42d062
smavros <stemavros@gmail.com>
2017-07-13 10:01:42 +0200

    finished optimizers test - removal of *_N_Iterations_Log() from Parameters section at API.md - global test values in optimizers test - optimizers test input file in test dir

--- core/test/test_optimizers.cpp
+++ core/test/test_optimizers.cpp
@@ -14,12 +14,19 @@ TEST_CASE( "Optimizers testing", "[optimizers]" )
 
   // optimizers and outputs
   std::vector<const char *>  optimizers { "SIB", 
-                                          "SIB2", 
-                                          "VP",
-                                          "Heun" };   // optimizers to be tested
+                                          "SIB2",   // TODO: remove in the future 
+                                          "VP"
+                                          //"NCG",
+                                          //"Heun"
+                                        };   // optimizers to be tested
+  
+  // expected values
+  float Energy = -5094.5556640625;
+  std::vector<float> Magnetization{ .0, .0, 0.644647 };
+
   int index;    // index of optimizer in optimizers list  
-  std::vector<float>        energy( optimizers.size(), .0 );      // energy output
-  std::vector<float [3]>    magnetization( optimizers.size() );   // magnetization output
+  scalar energy;    // enery output
+  std::vector<float> magnetization{ .0, .0, .0 };   // magnetization output
   
   // simulation parameters
   const char * method = "LLG";
@@ -29,7 +36,7 @@ TEST_CASE( "Optimizers testing", "[optimizers]" )
   // skyrmion characteristics
   float radius  = 5;
   int order     = 1;
-  float phase   = -90;
+  float phase   = 0;
   bool updown   = false,
        achiral  = false, 
        rl       = false,
@@ -48,7 +55,7 @@ TEST_CASE( "Optimizers testing", "[optimizers]" )
     index = find( optimizers.begin(), optimizers.end(), opt ) - optimizers.begin();
     auto state = std::shared_ptr<State>( State_Setup( inputfile ), State_Delete );
     
-    // put a skyrmion with phase -90 in the center of the space
+    // put a skyrmion with phase 0 in the center of the space
     Geometry_Get_Center( state.get(), position );
     Configuration_Skyrmion( state.get(), radius, order, phase, updown, achiral, rl, position,
                             r_cut_rectangular, r_cut_cylindrical, r_cut_spherical, inverted );
@@ -58,28 +65,15 @@ TEST_CASE( "Optimizers testing", "[optimizers]" )
     Simulation_PlayPause( state.get(), method, opt, n_iter, n_iter_log );
     
     // save energy and magnetization
-    energy[ index ] = System_Get_Energy( state.get() );
-    Quantity_Get_Magnetization( state.get(), magnetization[ index ] );
-  
-    INFO( opt );    // log the optimizer's name in case of failure
-    REQUIRE( energy[ index ] != .0 );                       // energy must be different than 0
-    REQUIRE( magnetization[ index ][0] == Approx( .0 ) );   // x-axis magnetization approx 0
-    REQUIRE( magnetization[ index ][1] == Approx( .0 ) );   // y-axis magnetization approx 0
-  }
-  
-  // check results between different optimizers
-  for (int i=0; i<optimizers.size(); i++)
-  {
-    for (int k=i+1; k<optimizers.size(); k++ )
-    {
-      // log the name of the optimizers under comparison
-      std::string opt1( optimizers[i] );
-      std::string opt2( optimizers[k] );
-      INFO( "While comparing " + opt1 + " - " + opt2 );
-      
-      // compare energy and magnetization along z-axis
-      REQUIRE( energy[i] == energy[k] );                        // equal energy
-      REQUIRE( magnetization[i][2] == magnetization[k][2] );    // equal magnetization
-    }
-  }
+    energy = System_Get_Energy( state.get() );
+    Quantity_Get_Magnetization( state.get(), magnetization.data() );
+        
+    // log the name of the optimizer
+    INFO( opt );
+
+    // check the values of energy and magnetization
+    REQUIRE( energy == Approx( Energy ) );
+    for (int j=0; j<3; j++)
+      REQUIRE( magnetization[j] == Approx( Magnetization[j] ) );
+  }    
 }
