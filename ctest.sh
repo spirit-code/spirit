ce7b0dfbfa289169be67288a694bc8d7252e2d76
Gideon Müller <gideon.mueller.91@outlook.com>
2017-08-11 11:47:41 +0200

    Re-simpliefied ctest.sh
    passing $@ fixes the problem of e.g. `-I 3,3` not reaching `ctest` when calling `./ctest.sh -I 3,3`

--- ctest.sh
+++ ctest.sh
@@ -1,24 +1,2 @@
-#!/bin/bash
-
 cd build
-
-# allow argument -I for running specific tests
-while getopts ":I:" opt; do
-    case $opt in
-        I)
-            ctest -I $OPTARG --output-on-failure 
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
-            exit 1
-            ;;
-        :)
-            ctest --output-on-failure
-            ;;
-    esac
-done
-
-# if there is no -I flag just call ctest
-if  [ $OPTIND -eq 1 ];
-    then ctest --output-on-failure
-fi
+ctest --output-on-failure $@
