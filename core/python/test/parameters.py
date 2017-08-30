79491a684c453bf9d3107951e2ee9d71633a52ac
smavros <stemavros@gmail.com>
2017-08-30 11:19:37 +0200

    Improvements on parameters API - change normal's type in C API to static arrays - added missing functions to python API - changed python API functions to pythonic interface - added unit tests for parameters.py - modified the corresponding python API documentation

--- core/python/test/parameters.py
+++ core/python/test/parameters.py
@@ -11,3 +11,120 @@ from spirit import parameters
 import unittest
 
 ##########
+
+cfgfile = "core/test/input/fd_neighbours.cfg"   # Input File
+p_state = state.setup(cfgfile)                  # State setup
+
+class TestParameters(unittest.TestCase):
+    
+    def setUp(self):
+        ''' Setup a p_state and copy it to Clipboard'''
+        self.p_state = p_state
+        
+class LLG_set_get(TestParameters):
+    
+    # XXX: what about output functions? 
+    
+    def test_LLG_N_iterations(self):
+        N_set = 100
+        Nlog_set = 100
+        parameters.Set_LLG_N_Iterations(self.p_state, N_set, Nlog_set)          # try set
+        N_get, Nlog_get = parameters.Get_LLG_N_Iterations(self.p_state, 0, 0)   # try get
+        self.assertEqual( N_set, N_get )
+        self.assertEqual( Nlog_set, Nlog_get )
+        
+    def test_LLG_direct_minimization(self):
+        parameters.Set_LLG_Direct_Minimization(self.p_state, True)      # try set
+        ret = parameters.Get_LLG_Direct_Minimization(self.p_state)      # try get
+        self.assertEqual( ret, True )
+        parameters.Set_LLG_Direct_Minimization(self.p_state, False)     # try set
+        ret = parameters.Get_LLG_Direct_Minimization(self.p_state)      # try get
+        self.assertEqual( ret, False )
+    
+    def test_LLG_convergence(self):
+        conv_set = 1.5e-3
+        parameters.Set_LLG_Convergence(self.p_state, conv_set)          # try set
+        conv_get = parameters.Get_LLG_Convergence(self.p_state)         # try get
+        self.assertAlmostEqual(conv_get, conv_set)
+    
+    def test_LLG_timestep(self):
+        dt_set = 1.5e-2
+        parameters.Set_LLG_Time_Step(self.p_state, dt_set)      # try set
+        dt_get = parameters.Get_LLG_Time_Step(self.p_state)     # try get
+        self.assertAlmostEqual(dt_set, dt_get)
+    
+    def test_LLG_damping(self):
+        lambda_set = 0.015
+        parameters.Set_LLG_Damping(self.p_state, lambda_set)        # try set
+        lambda_get = parameters.Get_LLG_Damping(self.p_state)       # try get
+        self.assertAlmostEqual(lambda_get, lambda_set)
+    
+    def test_LLG_STT(self):
+        use_grad_set = True
+        mag_set = 0.015
+        direction_set = [1., 0., 0.]        # NOTE: It will not work with tuples
+        parameters.Set_LLG_STT(self.p_state, use_grad_set, mag_set, direction_set )     # try set
+        stt_get = parameters.Get_LLG_STT(self.p_state)                                  # try get
+        self.assertEqual(stt_get[2], use_grad_set)
+        self.assertAlmostEqual(stt_get[0], mag_set)
+        self.assertAlmostEqual(stt_get[1], direction_set)
+    
+    def test_LLG_temperature(self):
+        temp_set = 100
+        parameters.Set_LLG_Temperature(self.p_state, temp_set)      # try set
+        temp_get = parameters.Get_LLG_Temperature(self.p_state)     # try get
+        self.assertAlmostEqual(temp_set, temp_get)
+
+class GNEB_set_get(TestParameters):
+    
+    def test_GNEB_N_Iterations(self):
+        N_set = 100
+        Nlog_set = 100
+        parameters.Set_GNEB_N_Iterations(self.p_state, N_set, Nlog_set)          # try set
+        N_get, Nlog_get = parameters.Get_GNEB_N_Iterations(self.p_state)   # try get
+        self.assertEqual( N_set, N_get )
+        self.assertEqual( Nlog_set, Nlog_get )
+
+    def test_GNEB_Convergence(self):
+        conv_set = 1.5e-3
+        parameters.Set_GNEB_Convergence(self.p_state, conv_set)          # try set
+        conv_get = parameters.Get_GNEB_Convergence(self.p_state)         # try get
+        self.assertAlmostEqual(conv_get, conv_set)
+    
+    # def test_GNEB_Spring_Constant(self):
+    #     k_set = 0.15
+    #     parameters.Set_GNEB_Spring_Constant(self.p_state, k_set)        # try set
+    #     k_get = parameters.Get_GNEB_Spring_Constant(self.p_state)       # try get
+    #     self.assertAlmostEqual(k_set, k_get)
+    
+    def test_GNEB_Climbing_Falling(self):
+        img_type_set = 1
+        parameters.Set_GNEB_Climbing_Falling(self.p_state, img_type_set)
+        cf = parameters.Get_GNEB_Climbing_Falling(self.p_state)
+        self.assertAlmostEqual(img_type_set, cf)
+    
+    def test_GNEB_Trivial_Image_Type(self):
+        # NOTE: this test is trivial since we cannot get the image type
+        parameters.Set_GNEB_Image_Type_Automatically(self.p_state)
+    
+    def test_GNEB_N_Energy_Interpolations(self):
+        # NOTE: this tests only the wrapping of the function since we cannot know the right value
+        E_inter = parameters.Get_GNEB_N_Energy_Interpolations(self.p_state)
+        self.assertTrue(E_inter > 0)
+    
+#########
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(LLG_set_get))
+    suite.addTest(unittest.makeSuite(GNEB_set_get))
+    return suite
+
+suite = suite()
+
+runner = unittest.TextTestRunner()
+success = runner.run(suite).wasSuccessful()
+
+state.delete( p_state )                         # Delete State
+
+sys.exit(not success)
