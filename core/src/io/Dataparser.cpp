339405c6bf3ce5200ef38935fbfca7a01f0520ef
smavros <stemavros@gmail.com>
2018-02-27 15:56:12 +0100

    Adjust the n_modes of the image for input with different number of modes

--- core/src/io/Dataparser.cpp
+++ core/src/io/Dataparser.cpp
@@ -304,7 +304,11 @@ namespace IO
             auto& geometry = *image->geometry;
            
             iFile_OVF ifile_ovf( filename, format );
-            ifile_ovf.read_eigenmodes( image->modes, geometry ); 
+            ifile_ovf.read_eigenmodes( image->modes, geometry );
+            
+            // if the modes vector was reseized adjust the n_modes value
+            if ( image->modes.size() != image->ema_parameters->n_modes )
+                image->ema_parameters->n_modes = image->modes.size();
         }
         else
         {
