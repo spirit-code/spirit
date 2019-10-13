f7e3ca41627ea628194e5571db0b52180be7ddc7
Moritz Sallermann <m.sallermann@fz-juelich.de>
2019-10-13 13:03:16 +0000

    UI-CPP: Added 'Lyra' command line argument parser.

---
+++ ui-cpp/thirdparty/Lyra/include/lyra/lyra.hpp
@@ -0,0 +1,19 @@
+// Copyright 2018-2019 Rene Rivera
+// Copyright 2017 Two Blue Cubes Ltd. All rights reserved.
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef LYRA_LYRA_HPP
+#define LYRA_LYRA_HPP
+
+#include "lyra/arg.hpp"
+#include "lyra/cli_parser.hpp"
+#include "lyra/exe_name.hpp"
+#include "lyra/help.hpp"
+#include "lyra/opt.hpp"
+#include "lyra/parser.hpp"
+#include "lyra/parser_result.hpp"
+#include "lyra/version.hpp"
+
+#endif // LYRA_HPP_INCLUDED
