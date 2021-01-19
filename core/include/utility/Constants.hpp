2b7d5d74c6e8ad478aa2d4f7531643a1be86c8ea
G. P. Mueller <gpmueller@outlook.com>
2021-01-20 00:21:05 +0100

    Core: format core/include/utility and core/src/utility files. Also improved include guards. Also removed some unneeded includes. Also replaced some `using namespace`s with more specific statements (e.g. `using Utility::Log_Level`).

--- core/include/utility/Constants.hpp
+++ core/include/utility/Constants.hpp
@@ -1,77 +1,87 @@
 #pragma once
-#ifndef UTILITY_CONSTANTS_H
-#define UTILITY_CONSTANTS_H
+#ifndef SPIRIT_UTILITY_CONSTANTS_HPP
+#define SPIRIT_UTILITY_CONSTANTS_HPP
 
 namespace Utility
 {
-    // Constants by convention:
-    //      Energy scale: Millielctronvolts
-    //      Time scale: Picoseconds
-    //      Magnetic fields scale: Tesla
-    namespace Constants
-    {
-        // The Bohr Magneton [meV/T]
-        double const mu_B = 0.057883817555;
 
-        // The vacuum permeability [T^2 m^3 / meV]
-        double const mu_0 = 2.0133545*1e-28;
+/*
+Constants by convention:
+- Energy scale: Millielctronvolts
+- Time scale: Picoseconds
+- Magnetic fields scale: Tesla
+*/
+namespace Constants
+{
 
-        // The Boltzmann constant [meV/K]
-        double const k_B  = 0.08617330350;
+// The Bohr Magneton [meV/T]
+double const mu_B = 0.057883817555;
 
-        // Planck constant [meV*ps/rad]
-        double const hbar = 0.6582119514;
+// The vacuum permeability [T^2 m^3 / meV]
+double const mu_0 = 2.0133545 * 1e-28;
 
-        // Gyromagnetic ratio of electron [rad/(ps*T)]
-        // Also gives the Larmor precession frequency for electron
-        double const gamma = 0.1760859644;
+// The Boltzmann constant [meV/K]
+double const k_B = 0.08617330350;
 
-        // Electron (Landé) g-factor = gamma * hbar / mu_B [unitless]
-        double const g_e = 2.00231930436182;
+// Planck constant [meV*ps/rad]
+double const hbar = 0.6582119514;
 
-        // Millirydberg [mRy/meV]
-        double const mRy = 1.0/13.605693009;
+// Gyromagnetic ratio of electron [rad/(ps*T)]
+// Also gives the Larmor precession frequency for electron
+double const gamma = 0.1760859644;
 
-        // erg [erg/meV]
-        double const erg = 6.2415091*1e14;
+// Electron (Landé) g-factor = gamma * hbar / mu_B [unitless]
+double const g_e = 2.00231930436182;
 
-        // Pi [rad]
-        double const Pi = 3.141592653589793238462643383279502884197169399375105820974;
+// Millirydberg [mRy/meV]
+double const mRy = 1.0 / 13.605693009;
 
-        // Pi/2 [rad]
-        double const Pi_2 = 1.570796326794896619231321691639751442098584699687552910487;
-    }
+// erg [erg/meV]
+double const erg = 6.2415091 * 1e14;
 
-    // Constants_mRy by convention:
-    //      Energy scale: Millirydberg
-    //      Time scale: Picoseconds
-    //      Magnetic fields scale: Tesla
-    namespace Constants_mRy
-    {
-        // The Bohr Magneton [mRy/T]
-        double const mu_B = Constants::mu_B / Constants::mRy;
+// Pi [rad]
+double const Pi = 3.141592653589793238462643383279502884197169399375105820974;
 
-        // The Boltzmann constant [mRy/K]
-        double const k_B  = Constants::k_B  / Constants::mRy;
+// Pi/2 [rad]
+double const Pi_2 = 1.570796326794896619231321691639751442098584699687552910487;
 
-        // Planck constant [mRy*ps/rad]
-        double const hbar = Constants::hbar / Constants::mRy;
+} // namespace Constants
 
-        // Millielectronvolt [meV/mRy]
-        double const meV = 1.0 / Constants::mRy;
+/*
+Constants_mRy by convention:
+- Energy scale: Millirydberg
+- Time scale: Picoseconds
+- Magnetic fields scale: Tesla
+*/
+namespace Constants_mRy
+{
 
-        // Gyromagnetic ratio of electron [rad/(ps*T)]
-        double const gamma = 0.1760859644;
+// The Bohr Magneton [mRy/T]
+double const mu_B = Constants::mu_B / Constants::mRy;
 
-        // Electron g-factor [unitless]
-        double const g_e = 2.00231930436182;
+// The Boltzmann constant [mRy/K]
+double const k_B = Constants::k_B / Constants::mRy;
 
-        // Pi [rad]
-        double const Pi = 3.141592653589793238462643383279502884197169399375105820974;
+// Planck constant [mRy*ps/rad]
+double const hbar = Constants::hbar / Constants::mRy;
 
-        // Pi/2 [rad]
-        double const Pi_2 = 1.570796326794896619231321691639751442098584699687552910487;
-    }
-}
+// Millielectronvolt [meV/mRy]
+double const meV = 1.0 / Constants::mRy;
+
+// Gyromagnetic ratio of electron [rad/(ps*T)]
+double const gamma = 0.1760859644;
+
+// Electron g-factor [unitless]
+double const g_e = 2.00231930436182;
+
+// Pi [rad]
+double const Pi = 3.141592653589793238462643383279502884197169399375105820974;
+
+// Pi/2 [rad]
+double const Pi_2 = 1.570796326794896619231321691639751442098584699687552910487;
+
+} // namespace Constants_mRy
+
+} // namespace Utility
 
 #endif
--- core/src/utility/Configuration_Chain.cpp
+++ core/src/utility/Configuration_Chain.cpp
@@ -1,60 +1,61 @@
+#include <data/Spin_System.hpp>
+#include <engine/Vectormath.hpp>
 #include <utility/Configuration_Chain.hpp>
 #include <utility/Configurations.hpp>
 #include <utility/Constants.hpp>
-#include <data/Spin_System.hpp>
-#include <engine/Vectormath.hpp>
 
 #include <Eigen/Dense>
 
-#include <random>
 #include <iostream>
+#include <random>
 #include <string>
 #include <vector>
 
-
 namespace Utility
 {
-    namespace Configuration_Chain
+namespace Configuration_Chain
+{
+
+void Add_Noise_Temperature( std::shared_ptr<Data::Spin_System_Chain> c, int idx_1, int idx_2, scalar temperature )
+{
+    for( int img = idx_1 + 1; img <= idx_2 - 1; ++img )
     {
-        void Add_Noise_Temperature(std::shared_ptr<Data::Spin_System_Chain> c, int idx_1, int idx_2, scalar temperature)
+        Configurations::Add_Noise_Temperature( *c->images[img], temperature, img );
+    }
+}
+
+void Homogeneous_Rotation( std::shared_ptr<Data::Spin_System_Chain> c, int idx_1, int idx_2 )
+{
+    auto & spins_1 = *c->images[idx_1]->spins;
+    auto & spins_2 = *c->images[idx_2]->spins;
+
+    scalar angle, rot_angle;
+    Vector3 rot_axis;
+
+    for( int i = 0; i < c->images[0]->nos; ++i )
+    {
+        rot_angle = Engine::Vectormath::angle( spins_1[i], spins_2[i] );
+        rot_axis  = spins_1[i].cross( spins_2[i] ).normalized();
+
+        // If they are not strictly parallel we can rotate
+        if( rot_angle > 1e-8 )
         {
-            for (int img = idx_1 + 1; img <= idx_2 - 1; ++img)
+            for( int img = idx_1 + 1; img < idx_2; ++img )
             {
-                Configurations::Add_Noise_Temperature(*c->images[img], temperature, img);
+                angle = rot_angle * scalar( img - idx_1 ) / scalar( idx_2 - idx_1 );
+                Engine::Vectormath::rotate( spins_1[i], rot_axis, angle, ( *c->images[img]->spins )[i] );
             }
         }
-
-        void Homogeneous_Rotation(std::shared_ptr<Data::Spin_System_Chain> c, int idx_1, int idx_2)
+        // Otherwise we simply leave the spin untouched
+        else
         {
-            auto& spins_1 = *c->images[idx_1]->spins;
-            auto& spins_2 = *c->images[idx_2]->spins;
-
-            scalar angle, rot_angle;
-            Vector3 rot_axis;
-
-            for( int i = 0; i < c->images[0]->nos; ++i )
+            for( int img = idx_1 + 1; img < idx_2; ++img )
             {
-                rot_angle = Engine::Vectormath::angle(spins_1[i], spins_2[i]);
-                rot_axis = spins_1[i].cross(spins_2[i]).normalized();
-
-                // If they are not strictly parallel we can rotate
-                if( rot_angle > 1e-8 )
-                {
-                    for( int img = idx_1+1; img < idx_2; ++img )
-                    {
-                        angle = rot_angle*scalar(img-idx_1)/scalar(idx_2 - idx_1);
-                        Engine::Vectormath::rotate(spins_1[i], rot_axis, angle, (*c->images[img]->spins)[i]);
-                    }
-                }
-                // Otherwise we simply leave the spin untouched
-                else
-                {
-                    for( int img = idx_1+1; img < idx_2; ++img )
-                    {
-                        (*c->images[img]->spins)[i] = spins_1[i];
-                    }
-                }
+                ( *c->images[img]->spins )[i] = spins_1[i];
             }
         }
-    }//end namespace Configuration_Chain
-}//end namespace Utility
+    }
+}
+
+} // namespace Configuration_Chain
+} // namespace Utility
