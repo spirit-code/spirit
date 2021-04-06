b8e36979725880a1789479c108d5032768734250
Gideon <g.mueller@fz-juelich.de>
2021-04-06 17:44:49 +0200

    Minor bugfix: use GLFW instead of ImGui for hiding cursor. This applies mainly to the interaction modes. Not sure if an something was changed with an ImGui update, but the current solution using `io.MouseDrawCursor` did not seem to work correctly anymore.

--- ui-cpp/ui-imgui/src/main_window.cpp
+++ ui-cpp/ui-imgui/src/main_window.cpp
@@ -930,10 +930,10 @@ void MainWindow::draw_imgui( int display_w, int display_h )
     if( ui_shared_state.interaction_mode != UiSharedState::InteractionMode::REGULAR )
     {
         if( ImGui::IsWindowHovered( ImGuiHoveredFlags_AnyWindow ) )
-            io.MouseDrawCursor = true;
+            glfwSetInputMode( glfw_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL );
         else
         {
-            io.MouseDrawCursor = false;
+            glfwSetInputMode( glfw_window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN );
 
             ImU32 color = IM_COL32( 0, 0, 0, 255 );
             if( ui_shared_state.interaction_mode == UiSharedState::InteractionMode::DRAG )
@@ -998,7 +998,7 @@ void MainWindow::draw_imgui( int display_w, int display_h )
         ImGui::PopStyleVar();
     }
     else
-        io.MouseDrawCursor = true;
+        glfwSetInputMode( glfw_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL );
 
     // ----------------
 
