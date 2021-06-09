fb00b96a40ba3d501905bd3617fb445634ba78b7
Moritz <m.sallermann@fz-juelich.de>
2021-06-09 13:37:00 +0200

    Incorporated upstream changes in core/utility

--- core/include/utility/Configurations.hpp
+++ core/include/utility/Configurations.hpp
@@ -1,57 +1,72 @@
 #pragma once
-#ifndef UTILITY_CONFIGURATIONS_H
-#define UTILITY_CONFIGURATIONS_H
+#ifndef SPIRIT_UTILITY_CONFIGURATIONS_HPP
+#define SPIRIT_UTILITY_CONFIGURATIONS_HPP
 
 #include "Spirit_Defines.h"
 #include <data/Spin_System.hpp>
 
-#include <vector>
-#include <random>
 #include <functional>
+#include <random>
+#include <vector>
 
 namespace Utility
 {
-    namespace Configurations
-    {
-        // Default filter function
-        typedef std::function< bool(const Vector3&, const Vector3&) > filterfunction;
-        filterfunction const defaultfilter = [](const Vector3& spin, const Vector3& pos)->bool { return true; };
-        void filter_to_mask(const vectorfield & spins, const vectorfield & positions, filterfunction filter, intfield & mask);
+namespace Configurations
+{
 
-        // TODO: replace the Spin_System references with smart pointers??
+// Default filter function
+using filterfunction               = std::function<bool( const Vector3 &, const Vector3 & )>;
+const filterfunction defaultfilter = []( const Vector3 & spin, const Vector3 & pos ) -> bool { return true; };
+void filter_to_mask( const vectorfield & spins, const vectorfield & positions, filterfunction filter, intfield & mask );
 
-        void Move(vectorfield& configuration, const Data::Geometry & geometry, int da, int db, int dc);
+// TODO: replace the Spin_System references with smart pointers??
 
-        // Insert data in certain region
-        void Insert(Data::Spin_System &s, const vectorfield& configuration, int shift = 0, filterfunction filter = defaultfilter);
+void Move( vectorfield & configuration, const Data::Geometry & geometry, int da, int db, int dc );
 
-        // orients all spins with x>pos into the direction of the v
-        void Domain(Data::Spin_System &s, Vector3 direction, filterfunction filter=defaultfilter);
+// Insert data in certain region
+void Insert(
+    Data::Spin_System & s, const vectorfield & configuration, int shift = 0, filterfunction filter = defaultfilter );
 
-        // points all Spins in random directions
-        void Random(Data::Spin_System &s, filterfunction filter=defaultfilter, bool external = false);
-        // Add temperature-scaled random noise to a system
-        void Add_Noise_Temperature(Data::Spin_System & s, scalar temperature, int delta_seed=0, filterfunction filter=defaultfilter);
+// orients all spins with x>pos into the direction of the v
+void Domain( Data::Spin_System & s, Vector3 direction, filterfunction filter = defaultfilter );
 
-        // Creates a toroid
-        void Hopfion(Data::Spin_System & s, Vector3 pos, scalar r, int order=1, filterfunction filter=defaultfilter);
+// points all Spins in random directions
+void Random( Data::Spin_System & s, filterfunction filter = defaultfilter, bool external = false );
 
-        // Creates a Skyrmion
-        void Skyrmion(Data::Spin_System & s, Vector3 pos, scalar r, scalar order, scalar phase, bool upDown, bool achiral, bool rl, bool experimental, filterfunction filter=defaultfilter);
+// Add temperature-scaled random noise to a system
+void Add_Noise_Temperature(
+    Data::Spin_System & s, scalar temperature, int delta_seed = 0, filterfunction filter = defaultfilter );
 
-        // Creates a Skyrmion, following the circular domain wall ("swiss knife") profile
-        void DW_Skyrmion(Data::Spin_System & s, Vector3 pos, scalar dw_radius, scalar dw_width, scalar order, scalar phase, bool upDown, bool achiral, bool rl, filterfunction filter=defaultfilter);
+// Creates a toroid
+void Hopfion( Data::Spin_System & s, Vector3 pos, scalar r, int order = 1, filterfunction filter = defaultfilter );
 
-        // Spin Spiral
-        void SpinSpiral(Data::Spin_System & s, std::string direction_type, Vector3 q, Vector3 axis, scalar theta, filterfunction filter=defaultfilter);
-        // 2q Spin Spiral
-        void SpinSpiral(Data::Spin_System & s, std::string direction_type, Vector3 q1, Vector3 q2, Vector3 axis, scalar theta, filterfunction filter=defaultfilter);
+// Creates a Skyrmion
+void Skyrmion(
+    Data::Spin_System & s, Vector3 pos, scalar r, scalar order, scalar phase, bool upDown, bool achiral, bool rl,
+    bool experimental, filterfunction filter = defaultfilter );
 
-        // Set atom types within a region of space
-        void Set_Atom_Types(Data::Spin_System & s, int atom_type=0, filterfunction filter=defaultfilter);
-        // Set spins to be pinned
-        void Set_Pinned(Data::Spin_System & s, bool pinned, filterfunction filter=defaultfilter);
-    };//end namespace Configurations
-}//end namespace Utility
+// Creates a Skyrmion, following the circular domain wall ("swiss knife") profile
+void DW_Skyrmion(
+    Data::Spin_System & s, Vector3 pos, scalar dw_radius, scalar dw_width, scalar order, scalar phase, bool upDown,
+    bool achiral, bool rl, filterfunction filter = defaultfilter );
+
+// Spin Spiral
+void SpinSpiral(
+    Data::Spin_System & s, std::string direction_type, Vector3 q, Vector3 axis, scalar theta,
+    filterfunction filter = defaultfilter );
+
+// 2q Spin Spiral
+void SpinSpiral(
+    Data::Spin_System & s, std::string direction_type, Vector3 q1, Vector3 q2, Vector3 axis, scalar theta,
+    filterfunction filter = defaultfilter );
+
+// Set atom types within a region of space
+void Set_Atom_Types( Data::Spin_System & s, int atom_type = 0, filterfunction filter = defaultfilter );
+
+// Set spins to be pinned
+void Set_Pinned( Data::Spin_System & s, bool pinned, filterfunction filter = defaultfilter );
+
+} //  namespace Configurations
+} //  namespace Utility
 
 #endif
--- core/include/utility/Constants.hpp
+++ core/include/utility/Constants.hpp
@@ -1,137 +1,127 @@
 #pragma once
-#ifndef UTILITY_CONSTANTS_H
-#define UTILITY_CONSTANTS_H
+#ifndef SPIRIT_UTILITY_CONSTANTS_HPP
+#define SPIRIT_UTILITY_CONSTANTS_HPP
 
 namespace Utility
 {
-    /*
-        Constants by convention:
-            Spatial scale: Angstrom
-            Energy scale: Millielctronvolts
-            Time scale: Picoseconds
-            Magnetic fields scale: Tesla
-    */
-    namespace Constants
-    {
-        // The Bohr Magneton [meV/T]
-        double const mu_B = 0.057883817555;
 
-        // The vacuum permeability [T^2 m^3 / meV]
-        double const mu_0 = 2.0133545*1e-28;
+/*
+Constants by convention:
+- Energy scale: Millielctronvolts
+- Time scale: Picoseconds
+- Magnetic fields scale: Tesla
+*/
+namespace Constants
+{
 
-        // The Boltzmann constant [meV/K]
-        double const k_B  = 0.08617330350;
+// The Bohr Magneton [meV/T]
+double const mu_B = 0.057883817555;
 
-        // Planck constant [meV*ps/rad]
-        double const hbar = 0.6582119514;
+// The vacuum permeability [T^2 m^3 / meV]
+double const mu_0 = 2.0133545 * 1e-28;
 
-        // Gyromagnetic ratio of electron [rad/(ps*T)]
-        double const gamma = 0.1760859644;
+// The Boltzmann constant [meV/K]
+double const k_B = 0.08617330350;
 
-        // Electron (Landé) g-factor = gamma * hbar / mu_B [unitless]
-        double const g_e = 2.00231930436182;
+// Planck constant [meV*ps/rad]
+double const hbar = 0.6582119514;
 
-        // Millirydberg [mRy/meV]
-        double const mRy = 13.605693009;
+// Gyromagnetic ratio of electron [rad/(ps*T)]
+// Also gives the Larmor precession frequency for electron
+double const gamma = 0.1760859644;
 
-        // erg [erg/meV]
-        double const erg = 6.2415091*1e+14;
+// Electron (Landé) g-factor = gamma * hbar / mu_B [unitless]
+double const g_e = 2.00231930436182;
 
-        // Joule [Joule/meV]
-        double const Joule = 6.2415091*1e+21;
+// Millirydberg [mRy/meV]
+double const mRy = 1.0 / 13.605693009;
 
-        // Pi [rad]
-        double const Pi = 3.141592653589793238462643383279502884197169399375105820974;
+// erg [erg/meV]
+double const erg = 6.2415091 * 1e14;
 
-        // Pi/2 [rad]
-        double const Pi_2 = 1.570796326794896619231321691639751442098584699687552910487;
-    }
+// Joule [Joule/meV]
+double const Joule = 6.2415091 * 1e+21;
 
-    /*
-        Constants_mRy by convention:
-            Spatial scale: Angstrom
-            Energy scale: Millirydberg
-            Time scale: Picoseconds
-            Magnetic fields scale: Tesla
-    */
-    namespace Constants_mRy
-    {
-        // The Bohr Magneton [mRy/T]
-        double const mu_B = Constants::mu_B * Constants::mRy;
+// Pi [rad]
+double const Pi = 3.141592653589793238462643383279502884197169399375105820974;
 
-        // The vacuum permeability [T^2 m^3 / mRy]
-        double const mu_0 = Constants::mu_0 / Constants::mRy;
+// Pi/2 [rad]
+double const Pi_2 = 1.570796326794896619231321691639751442098584699687552910487;
 
-        // The Boltzmann constant [mRy/K]
-        double const k_B  = Constants::k_B  / Constants::mRy;
+} // namespace Constants
 
-        // Planck constant [mRy*ps/rad]
-        double const hbar = Constants::hbar * Constants::mRy;
+/*
+Constants_mRy by convention:
+- Energy scale: Millirydberg
+- Time scale: Picoseconds
+- Magnetic fields scale: Tesla
+*/
+namespace Constants_mRy
+{
 
-        // Gyromagnetic ratio of electron [rad/(ps*T)]
-        double const gamma = Constants::gamma;
+// The Bohr Magneton [mRy/T]
+double const mu_B = Constants::mu_B / Constants::mRy;
 
-        // Electron g-factor [unitless]
-        double const g_e = Constants::g_e;
+// The Boltzmann constant [mRy/K]
+double const k_B = Constants::k_B / Constants::mRy;
 
-        // Millielectronvolt [meV/mRy]
-        double const meV = 1.0 / Constants::mRy;
+// Planck constant [mRy*ps/rad]
+double const hbar = Constants::hbar / Constants::mRy;
 
-        // Joule [Joule/mRy]
-        double const mRy = Constants::Joule / Constants::mRy;
+// Millielectronvolt [meV/mRy]
+double const meV = 1.0 / Constants::mRy;
 
-        // erg [erg/mRy]
-        double const erg = Constants::erg / Constants::mRy;
+// Gyromagnetic ratio of electron [rad/(ps*T)]
+double const gamma = 0.1760859644;
 
-        // Pi [rad]
-        double const Pi = Constants::Pi;
+// Electron g-factor [unitless]
+double const g_e = 2.00231930436182;
 
-        // Pi/2 [rad]
-        double const Pi_2 = Constants::Pi_2;
-    }
+} // namespace Constants_mRy
 
-    /*
-        Constants by micromagnetic convention (SI units):
-            Spatial scale: meters
-            Energy scale: Joule
-            Time scale: seconds
-            Magnetic fields scale: Tesla
-    */
-    namespace Constants_Micromagnetic
-    {
-        // The Bohr Magneton [Joule/T]
-        double const mu_B = Constants::mu_B / Constants::Joule;
+/*
+Constants by micromagnetic convention (SI units):
+ - Spatial scale: meters
+ - Energy scale: Joule
+ - Time scale: seconds
+ - Magnetic fields scale: Tesla
+*/
+namespace Constants_Micromagnetic
+{
+// The Bohr Magneton [Joule/T]
+double const mu_B = Constants::mu_B / Constants::Joule;
 
-        // The vacuum permeability [T^2 m^3 / Joule]
-        double const mu_0 = Constants::mu_0 / Constants::Joule;
+// The vacuum permeability [T^2 m^3 / Joule]
+double const mu_0 = Constants::mu_0 / Constants::Joule;
 
-        // The Boltzmann constant [J/K]
-        double const k_B  = Constants::k_B * Constants::Joule;
+// The Boltzmann constant [J/K]
+double const k_B = Constants::k_B * Constants::Joule;
 
-        // Planck constant [J*s/rad]
-        double const hbar = Constants::hbar * Constants::Joule * 1e-12;
+// Planck constant [J*s/rad]
+double const hbar = Constants::hbar * Constants::Joule * 1e-12;
 
-        // Gyromagnetic ratio of electron [rad/(s*T)]
-        double const gamma = Constants::gamma * 1e+12;
+// Gyromagnetic ratio of electron [rad/(s*T)]
+double const gamma = Constants::gamma * 1e+12;
 
-        // Electron (Landé) g-factor = gamma * hbar / mu_B [unitless]
-        double const g_e = Constants::g_e;
+// Electron (Landé) g-factor = gamma * hbar / mu_B [unitless]
+double const g_e = Constants::g_e;
 
-        // meV [meV/Joule]
-        double const meV = 1.0 / Constants::Joule;
+// meV [meV/Joule]
+double const meV = 1.0 / Constants::Joule;
 
-        // Millirydberg [mRy/Joule]
-        double const mRy = Constants::mRy / Constants::Joule;
+// Millirydberg [mRy/Joule]
+double const mRy = Constants::mRy / Constants::Joule;
 
-        // erg [erg/Joule]
-        double const erg = Constants::erg / Constants::Joule;
+// erg [erg/Joule]
+double const erg = Constants::erg / Constants::Joule;
 
-        // Pi [rad]
-        double const Pi = Constants::Pi;
+// Pi [rad]
+double const Pi = Constants::Pi;
 
-        // Pi/2 [rad]
-        double const Pi_2 = Constants::Pi_2;
-    }
-}
+// Pi/2 [rad]
+double const Pi_2 = Constants::Pi_2;
+} // namespace Constants_Micromagnetic
+
+} // namespace Utility
 
 #endif
