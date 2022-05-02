8671e19ff321a794a08db77a84db13b692c60951
Moritz <m.sallermann@fz-juelich.de>
2022-05-02 13:14:12 +0200

    Solver_Kernels: Small change in lbfgs_get_searchdir
     When compiling for CUDA11, one of the Kernels was trying to use the [] operator of std::vector

--- core/include/engine/Solver_Kernels.hpp
+++ core/include/engine/Solver_Kernels.hpp
@@ -129,9 +129,9 @@ void lbfgs_get_searchdir(
             {
                 auto q = q_vec[img].data();
                 auto a = alpha.data();
-                auto d = delta_grad[img].data();
+                auto d = delta_grad[img][c_ind].data();
                 Backend::par::apply(
-                    nos, [c_ind, q, a, d] SPIRIT_LAMBDA( int idx ) { q[idx] += -a[c_ind] * d[c_ind][idx]; } );
+                    nos, [c_ind, q, a, d] SPIRIT_LAMBDA( int idx ) { q[idx] += -a[c_ind] * d[idx]; } );
             }
         }
 
