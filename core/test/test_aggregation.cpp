750e56e678276b84ddd4669967b01130ac302907
puerling <t.puerling@fz-juelich.de>
2023-11-22 15:07:42 +0100

    refactor(modular Hamiltonian): extend and update tests
    Update the `test_physics` framework test to test the individual
    interaction terms and not the full Hamiltonian.
    
    The test of the aggregation properties is now done by a new
    `test_aggregation` framework test.

---
+++ core/test/test_aggregation.cpp
@@ -0,0 +1,143 @@
+#include <Spirit/Configurations.h>
+#include <Spirit/Constants.h>
+#include <Spirit/Geometry.h>
+#include <Spirit/Hamiltonian.h>
+#include <Spirit/Parameters_LLG.h>
+#include <Spirit/Simulation.h>
+#include <Spirit/State.h>
+#include <Spirit/System.h>
+#include <Spirit/Version.h>
+#include <data/State.hpp>
+#include <engine/Vectormath.hpp>
+
+#include "catch.hpp"
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+using Catch::Matchers::WithinAbs;
+
+// Reduce required precision if float accuracy
+#ifdef SPIRIT_SCALAR_TYPE_DOUBLE
+constexpr scalar epsilon_2 = 1e-10;
+constexpr scalar epsilon_3 = 1e-12;
+constexpr scalar epsilon_4 = 1e-12;
+constexpr scalar epsilon_5 = 1e-6;
+constexpr scalar epsilon_6 = 1e-7;
+#else
+constexpr scalar epsilon_2 = 1e-2;
+constexpr scalar epsilon_3 = 1e-3;
+constexpr scalar epsilon_4 = 1e-4;
+constexpr scalar epsilon_5 = 1e-5;
+constexpr scalar epsilon_6 = 1e-6;
+#endif
+
+TEST_CASE( "Ensure that Hamiltonian is really just an aggregator", "[aggregation]" )
+{
+    // Hamiltonians to be tested
+    std::vector<const char *> hamiltonian_input_files{
+        "core/test/input/fd_gaussian.cfg",
+        "core/test/input/fd_pairs.cfg",
+        "core/test/input/fd_neighbours.cfg",
+    };
+
+    for( const auto * input_file : hamiltonian_input_files )
+    {
+        INFO( " Testing" << input_file );
+
+        auto state = std::shared_ptr<State>( State_Setup( input_file ), State_Delete );
+        Configuration_Random( state.get() );
+        const auto & spins = *state->active_image->spins;
+        auto & hamiltonian = state->active_image->hamiltonian;
+        auto nos           = spins.size();
+
+        if( hamiltonian->getActiveInteractionsSize() == 0 )
+        {
+            CAPTURE( fmt::format( " Warning: input file \"{}\" didn't specify any interaction to test.", input_file ) );
+        }
+
+        auto active_interactions = hamiltonian->getActiveInteractions();
+        auto aggregator          = [&active_interactions]( const auto init, const auto & f )
+        { return std::accumulate( std::begin( active_interactions ), std::end( active_interactions ), init, f ); };
+
+        scalar energy_hamiltonian = hamiltonian->Energy( spins );
+        scalar energy_aggregated  = aggregator(
+            0.0,
+            [&spins]( const scalar v, const auto & interaction ) -> scalar
+            { return v + interaction->Energy( spins ); } );
+
+        INFO( "Hamiltonian::Energy" )
+        INFO( "[total], epsilon = " << epsilon_2 << "\n" );
+        INFO( "Energy (Hamiltonian) = " << energy_hamiltonian << "\n" );
+        INFO( "Energy (aggregated)  = " << energy_aggregated << "\n" );
+        REQUIRE_THAT( energy_hamiltonian, WithinAbs( energy_aggregated, epsilon_2 ) );
+
+        scalarfield energy_per_spin_hamiltonian{}; // resize and clear should be handled by the hamiltonian
+        hamiltonian->Energy_per_Spin( spins, energy_per_spin_hamiltonian );
+        scalarfield energy_per_spin_aggregated = aggregator(
+            scalarfield( nos, 0 ),
+            [&spins]( const scalarfield & v, const auto & interaction ) -> scalarfield
+            {
+                auto energy_per_spin = scalarfield( spins.size(), 0 );
+                interaction->Energy_per_Spin( spins, energy_per_spin );
+#pragma omp parallel for
+                for( std::size_t i = 0; i < spins.size(); ++i )
+                    energy_per_spin[i] += v[i];
+
+                return energy_per_spin;
+            } );
+
+        for( int i = 0; i < state->nos; i++ )
+        {
+            INFO( "Hamiltonian::Energy_per_Spin" )
+            INFO( "i = " << i << ", epsilon = " << epsilon_2 << "\n" );
+            INFO( "Energy (Hamiltonian)   = " << energy_per_spin_hamiltonian[i] << "\n" );
+            INFO( "Energy (aggregated) = " << energy_per_spin_aggregated[i] << "\n" );
+            REQUIRE_THAT( energy_per_spin_hamiltonian[i], WithinAbs( energy_per_spin_aggregated[i], epsilon_2 ) );
+        }
+
+        vectorfield gradient_hamiltonian{}; // resize and clear should be handled by the hamiltonian
+        hamiltonian->Gradient( spins, gradient_hamiltonian );
+        vectorfield gradient_aggregated = aggregator(
+            vectorfield( nos, Vector3::Zero() ),
+            [&spins]( const vectorfield & v, const auto & interaction ) -> vectorfield
+            {
+                auto gradient = vectorfield( spins.size(), Vector3::Zero() );
+                interaction->Gradient( spins, gradient );
+                Engine::Vectormath::add_c_a( 1.0, v, gradient );
+                return gradient;
+            } );
+
+        for( int i = 0; i < state->nos; i++ )
+        {
+            INFO( "Hamiltonian::Gradient" )
+            INFO( "i = " << i << ", epsilon = " << epsilon_2 << "\n" );
+            INFO( "Gradient (Hamiltonian)   = " << gradient_hamiltonian[i] << "\n" );
+            INFO( "Gradient (aggregated) = " << gradient_aggregated[i] << "\n" );
+            REQUIRE( gradient_hamiltonian[i].isApprox( gradient_aggregated[i], epsilon_2 ) );
+        }
+
+        scalar energy_combined_hamiltonian = 0;
+        vectorfield gradient_combined_hamiltonian{};
+        hamiltonian->Gradient_and_Energy( spins, gradient_combined_hamiltonian, energy_combined_hamiltonian );
+
+        for( int i = 0; i < state->nos; i++ )
+        {
+            INFO( "Hamiltonian::Gradient_and_Energy" )
+            INFO( "i = " << i << ", epsilon = " << epsilon_2 << "\n" );
+            INFO( "Gradient (combined)   = " << gradient_combined_hamiltonian[i] << "\n" );
+            INFO( "Gradient (aggregated) = " << gradient_aggregated[i] << "\n" );
+            REQUIRE( gradient_combined_hamiltonian[i].isApprox( gradient_aggregated[i], epsilon_2 ) );
+        }
+
+        INFO( "Hamiltonian::Gradient_and_Energy" )
+        INFO( "[total], epsilon = " << epsilon_2 << "\n" );
+        INFO( "Energy (combined)   = " << energy_combined_hamiltonian << "\n" );
+        INFO( "Energy (aggregated) = " << energy_aggregated << "\n" );
+        REQUIRE_THAT( energy_combined_hamiltonian, WithinAbs( energy_aggregated, epsilon_2 ) );
+    }
+}
--- core/test/test_physics.cpp
+++ core/test/test_physics.cpp
@@ -8,6 +8,7 @@
 #include <Spirit/System.h>
 #include <Spirit/Version.h>
 #include <data/State.hpp>
+#include <engine/Vectormath.hpp>
 
 #include "catch.hpp"
 
@@ -49,7 +50,7 @@ TEST_CASE( "Dynamics solvers should follow Larmor precession", "[physics]" )
     auto state = std::shared_ptr<State>( State_Setup( input_file ), State_Delete );
 
     // Set up one the initial direction of the spin
-    scalar init_direction[3] = { 1., 0., 0. };            // vec parallel to x-axis
+    scalar init_direction[3] = { 1., 0., 0. };           // vec parallel to x-axis
     Configuration_Domain( state.get(), init_direction ); // set spin parallel to x-axis
 
     // Assure that the initial direction is set
@@ -71,7 +72,7 @@ TEST_CASE( "Dynamics solvers should follow Larmor precession", "[physics]" )
 
     // Get time step of method
     scalar damping = 0.3;
-    scalar tstep    = Parameters_LLG_Get_Time_Step( state.get() );
+    scalar tstep   = Parameters_LLG_Get_Time_Step( state.get() );
     Parameters_LLG_Set_Damping( state.get(), damping );
 
     scalar dtg = tstep * Constants_gamma() / ( 1.0 + damping * damping );
@@ -104,15 +105,15 @@ TEST_CASE( "Dynamics solvers should follow Larmor precession", "[physics]" )
     }
 }
 
+// Hamiltonians to be tested
+static constexpr std::array hamiltonian_input_files{
+    "core/test/input/fd_pairs.cfg",
+    "core/test/input/fd_neighbours.cfg",
+    // "core/test/input/fd_gaussian.cfg", // TODO: issue with precision
+};
+
 TEST_CASE( "Finite difference and regular Hamiltonian should match", "[physics]" )
 {
-    // Hamiltonians to be tested
-    std::vector<const char *> hamiltonian_input_files{
-        "core/test/input/fd_pairs.cfg",
-        // "core/test/input/fd_neighbours",
-        // "core/test/input/fd_gaussian.cfg",
-    };
-
     for( const auto * input_file : hamiltonian_input_files )
     {
         INFO( " Testing " << input_file );
@@ -120,29 +121,146 @@ TEST_CASE( "Finite difference and regular Hamiltonian should match", "[physics]"
         auto state = std::shared_ptr<State>( State_Setup( input_file ), State_Delete );
         Configuration_Random( state.get() );
         const auto & spins = *state->active_image->spins;
+        auto & hamiltonian = state->active_image->hamiltonian;
 
         // Compare gradients
-        auto grad    = vectorfield( state->nos );
-        auto grad_fd = vectorfield( state->nos );
-        state->active_image->hamiltonian->Gradient_FD( spins, grad_fd );
-        state->active_image->hamiltonian->Gradient( spins, grad );
-        for( int i = 0; i < state->nos; i++ )
+        auto grad    = vectorfield( state->nos, Vector3::Zero() );
+        auto grad_fd = vectorfield( state->nos, Vector3::Zero() );
+        for( const auto & interaction : hamiltonian->getActiveInteractions() )
         {
-            INFO( "i = " << i << ", epsilon = " << epsilon_2 << "\n" );
-            INFO( "Gradient (FD) = " << grad_fd[i].transpose() << "\n" );
-            INFO( "Gradient      = " << grad[i].transpose() << "\n" );
-            REQUIRE( grad_fd[i].isApprox( grad[i], epsilon_2 ) );
+            Engine::Vectormath::fill( grad, Vector3::Zero() );
+            Engine::Vectormath::fill( grad_fd, Vector3::Zero() );
+            interaction->Gradient( spins, grad );
+            interaction->Gradient_FD( spins, grad_fd );
+            INFO( "Interaction: " << interaction->Name() << "\n" );
+            for( int i = 0; i < state->nos; i++ )
+            {
+                INFO( "i = " << i << ", epsilon = " << epsilon_2 << "\n" );
+                INFO( "Gradient (FD) = " << grad_fd[i].transpose() << "\n" );
+                INFO( "Gradient      = " << grad[i].transpose() << "\n" );
+                REQUIRE( grad_fd[i].isApprox( grad[i], epsilon_2 ) );
+            }
         }
 
         // Compare Hessians
         auto hessian    = MatrixX( 3 * state->nos, 3 * state->nos );
         auto hessian_fd = MatrixX( 3 * state->nos, 3 * state->nos );
-        state->active_image->hamiltonian->Hessian_FD( spins, hessian_fd );
-        state->active_image->hamiltonian->Hessian( spins, hessian );
-        INFO( "epsilon = " << epsilon_3 << "\n" );
-        INFO( "Hessian (FD) =\n" << hessian_fd << "\n" );
-        INFO( "Hessian      =\n" << hessian << "\n" );
-        REQUIRE( hessian_fd.isApprox( hessian, epsilon_3 ) );
+        for( const auto & interaction : hamiltonian->getActiveInteractions() )
+        {
+            hessian.setZero();
+            hessian_fd.setZero();
+
+            interaction->Hessian_FD( spins, hessian_fd );
+            interaction->Hessian( spins, hessian );
+            INFO( "Interaction: " << interaction->Name() << "\n" );
+            INFO( "epsilon = " << epsilon_3 << "\n" );
+            INFO( "Hessian (FD) =\n" << hessian_fd << "\n" );
+            INFO( "Hessian      =\n" << hessian << "\n" );
+            REQUIRE( hessian_fd.isApprox( hessian, epsilon_3 ) );
+        }
+    }
+}
+
+TEST_CASE( "Energy calculations should be consistent with themselves", "[physics]" )
+{
+    for( const auto * input_file : hamiltonian_input_files )
+    {
+        INFO( " Testing " << input_file );
+
+        auto state = std::shared_ptr<State>( State_Setup( input_file ), State_Delete );
+        Configuration_Random( state.get() );
+        const auto & spins = *state->active_image->spins;
+        auto & hamiltonian = state->active_image->hamiltonian;
+
+        // Compare gradients
+        scalar energy_a         = 0;
+        scalar energy_b         = 0;
+        auto energy_per_spin    = scalarfield( state->nos );
+        auto energy_single_spin = scalarfield( state->nos );
+        for( const auto & interaction : hamiltonian->getActiveInteractions() )
+        {
+            energy_a = 0;
+            energy_b = 0;
+            Engine::Vectormath::fill( energy_per_spin, 0 );
+            Engine::Vectormath::fill( energy_single_spin, 0 );
+
+            energy_a = interaction->Energy( spins );
+            interaction->Energy_per_Spin( spins, energy_per_spin );
+            energy_b = Engine::Vectormath::sum( energy_per_spin );
+
+            INFO( "Interaction: " << interaction->Name() << "\n" );
+            INFO( "[total], epsilon = " << epsilon_2 << "\n" );
+            INFO( "Energy (method) = " << energy_a << "\n" );
+            INFO( "Energy (sum)    = " << energy_b << "\n" );
+            REQUIRE_THAT( energy_a, WithinAbs( energy_b, epsilon_2 ) );
+
+            for( int i = 0; i < state->nos; i++ )
+            {
+                energy_single_spin[i] = interaction->Energy_Single_Spin( i, spins );
+            }
+
+            const auto spin_order = interaction->spin_order().value_or( 1 );
+            const scalar c        = ( spin_order != 0 ) ? static_cast<scalar>( spin_order ) : 1.0;
+            for( int i = 0; i < state->nos; i++ )
+            {
+                INFO( "Interaction: " << interaction->Name() << "\n" );
+                INFO( "i = " << i << ", epsilon = " << epsilon_2 << "\n" );
+                INFO( "Energy (full)   = " << energy_per_spin[i] << "\n" );
+                INFO( "Energy (single) = " << energy_single_spin[i] << "\n" );
+                REQUIRE_THAT( c * energy_per_spin[i], WithinAbs( energy_single_spin[i], epsilon_2 ) );
+            }
+        }
+    }
+}
+TEST_CASE( "Energy calculations should be consistent with gradients", "[physics]" )
+{
+    for( const auto * input_file : hamiltonian_input_files )
+    {
+        INFO( " Testing " << input_file );
+
+        auto state = std::shared_ptr<State>( State_Setup( input_file ), State_Delete );
+        Configuration_Random( state.get() );
+        const auto & spins = *state->active_image->spins;
+        auto & hamiltonian = state->active_image->hamiltonian;
+        auto nos           = state->nos;
+
+        // Compare energy to energy from gradients
+        auto gradient              = vectorfield( nos );
+        auto energy_per_spin       = scalarfield( nos );
+        auto energy_from_gradients = scalarfield( nos );
+
+        auto * s = spins.data();
+        auto * g = gradient.data();
+        auto * e = energy_from_gradients.data();
+
+        for( const auto & interaction : hamiltonian->getActiveInteractions() )
+        {
+            auto spin_order = interaction->spin_order().value_or( 0 );
+            if( spin_order <= 0 )
+            {
+                continue;
+            }
+
+            const scalar c = 1.0 / spin_order;
+            Engine::Vectormath::fill( gradient, Vector3::Zero() );
+            Engine::Vectormath::fill( energy_per_spin, 0 );
+
+            interaction->Gradient( spins, gradient );
+
+            Engine::Backend::par::apply(
+                nos, [e, g, s, c] SPIRIT_LAMBDA( int idx ) { e[idx] = c * g[idx].dot( s[idx] ); } );
+
+            interaction->Energy_per_Spin( spins, energy_per_spin );
+
+            for( int i = 0; i < state->nos; i++ )
+            {
+                INFO( "Interaction: " << interaction->Name() << "\n" );
+                INFO( "i = " << i << ", epsilon = " << epsilon_2 << "\n" );
+                INFO( "Energy (direct)        = " << energy_per_spin[i] << "\n" );
+                INFO( "Energy (from gradient) = " << energy_from_gradients[i] << "\n" );
+                REQUIRE_THAT( energy_per_spin[i], WithinAbs( energy_from_gradients[i], epsilon_2 ) );
+            }
+        }
     }
 }
 
@@ -155,17 +273,19 @@ TEST_CASE( "Dipole-Dipole Interaction", "[physics]" )
     Configuration_Random( state.get() );
     auto & spins = *state->active_image->spins;
 
+    auto * ddi_interaction = state->active_image->hamiltonian->getInteraction<Engine::Interaction::DDI>();
+
     // FFT gradient and energy
-    auto grad_fft = vectorfield( state->nos );
-    state->active_image->hamiltonian->Gradient( spins, grad_fft );
+    auto grad_fft = vectorfield( state->nos, Vector3::Zero() );
+    ddi_interaction->Gradient( spins, grad_fft );
     auto energy_fft = state->active_image->hamiltonian->Energy( spins );
 
     // Direct (cutoff) gradient and energy
     auto n_periodic_images = std::vector<int>{ 4, 4, 4 };
     Hamiltonian_Set_DDI( state.get(), SPIRIT_DDI_METHOD_CUTOFF, n_periodic_images.data(), -1 );
-    auto grad_direct = vectorfield( state->nos );
-    state->active_image->hamiltonian->Gradient( spins, grad_direct );
-    auto energy_direct = state->active_image->hamiltonian->Energy( spins );
+    auto grad_direct = vectorfield( state->nos, Vector3::Zero() );
+    ddi_interaction->Gradient( spins, grad_direct );
+    auto energy_direct = ddi_interaction->Energy( spins );
 
     // Compare gradients
     for( int i = 0; i < state->nos; i++ )
