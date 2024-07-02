7cb9fb7ab9f7ed3349b34b08351bc3b1f8154755
puerling <t.puerling@fz-juelich.de>
2024-07-02 09:53:30 +0200

    fix(python-api): avoid `*` imports and use correct name of `parameters.ema`-method

--- core/python/spirit/parameters/__init__.py
+++ core/python/spirit/parameters/__init__.py
@@ -7,4 +7,4 @@ Change the parameters of the various methods, which can be used.
 
 __all__ = ["gneb", "llg", "mc", "ema", "mmf"]
 
-from spirit.parameters import *
+from spirit.parameters import gneb, llg, mc, ema, mmf
