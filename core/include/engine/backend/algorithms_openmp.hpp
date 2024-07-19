7a54c69f22388d73ea4c07d2791bb7fd7a8ad06e
puerling <t.puerling@fz-juelich.de>
2024-07-19 15:05:22 +0200

    fix(OpenMP): add generic reduce operations to OpenMP backend
    The code requires more than just reduction over `+` and `max`. To
    support the full range of reduction operation that the other backends
    can support we add a generic reduction operation for OpenMP.

--- core/include/engine/backend/algorithms_openmp.hpp
+++ core/include/engine/backend/algorithms_openmp.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #if defined( SPIRIT_USE_OPENMP ) && !defined( SPIRIT_USE_STDPAR )
+#include <engine/backend/Functor.hpp>
 #include <engine/backend/types.hpp>
 
 #include <iterator>
@@ -31,12 +32,40 @@ auto reduce_n( const ::execution::par_t &, ForwardIt first, Size n ) ->
 };
 
 template<typename ForwardIt, typename Size, typename T>
-auto reduce_n( const ::execution::par_t &, ForwardIt first, Size n, T init, Backend::plus<T>{} ) -> T
+auto reduce_n( const ::execution::par_t &, ForwardIt first, Size n, T init, Backend::max<T> ) -> T
+{
+#pragma omp parallel for reduction( max : init )
+    for( Size i = 0; i < n; ++i )
+    {
+        init = std::max( init, *( first + i ) );
+    }
+    return init;
+};
+
+template<typename ForwardIt, typename Size, typename T>
+auto reduce_n( const ::execution::par_t &, ForwardIt first, Size n, T init, Backend::plus<T> ) -> T
 {
 #pragma omp parallel for reduction( + : init )
     for( Size i = 0; i < n; ++i )
     {
         init += *( first + i );
+    }
+    return init;
+};
+
+template<typename ForwardIt, typename Size, typename T, typename BinaryReductionOp>
+auto reduce_n( const ::execution::par_t &, ForwardIt first, Size n, T init, BinaryReductionOp binary_op ) -> T
+{
+#pragma omp parallel
+    {
+        T local_result = zero_value<T>();
+#pragma omp for
+        for( Size i = 0; i < n; ++i )
+        {
+            local_result = binary_op( local_result, *( first + i ) );
+        }
+#pragma critical
+        init = binary_op( init, local_result );
     }
     return init;
 };
@@ -65,12 +94,54 @@ ForwardIt3 transform_n(
 }
 
 template<typename ForwardIt, typename Size, typename T, typename UnaryOp>
+T transform_reduce_n( const ::execution::par_t &, ForwardIt first, Size n, T init, Backend::max<T>, UnaryOp unary_op )
+{
+#pragma omp parallel for reduction( max : init )
+    for( Size i = 0; i < n; ++i )
+    {
+        init = std::max( init, unary_op( *( first + i ) ) );
+    }
+    return init;
+}
+
+template<typename ForwardIt, typename Size, typename T, typename UnaryOp>
 T transform_reduce_n( const ::execution::par_t &, ForwardIt first, Size n, T init, Backend::plus<T>, UnaryOp unary_op )
 {
 #pragma omp parallel for reduction( + : init )
     for( Size i = 0; i < n; ++i )
     {
         init += unary_op( *( first + i ) );
+    }
+    return init;
+}
+
+template<typename ForwardIt, typename Size, typename T, typename BinaryReductionOp, typename UnaryOp>
+T transform_reduce_n(
+    const ::execution::par_t &, ForwardIt first, Size n, T init, BinaryReductionOp reduce, UnaryOp unary_op )
+{
+#pragma omp parallel
+    {
+        T local_result = zero_value<T>();
+#pragma omp for
+        for( Size i = 0; i < n; ++i )
+        {
+            local_result = reduce( local_result, unary_op( *( first + i ) ) );
+        }
+#pragma critical
+        init = reduce( init, local_result );
+    }
+    return init;
+}
+
+template<typename ForwardIt1, typename Size, typename ForwardIt2, typename T, typename BinaryOp>
+T transform_reduce_n(
+    const ::execution::par_t &, ForwardIt1 first1, Size n, ForwardIt2 first2, T init, Backend::max<T>,
+    BinaryOp binary_op )
+{
+#pragma omp parallel for reduction( max : init )
+    for( Size i = 0; i < n; ++i )
+    {
+        init = std::max( init, binary_op( *( first1 + i ), *( first2 + i ) ) );
     }
     return init;
 }
@@ -84,6 +155,26 @@ T transform_reduce_n(
     for( Size i = 0; i < n; ++i )
     {
         init += binary_op( *( first1 + i ), *( first2 + i ) );
+    }
+    return init;
+}
+
+template<
+    typename ForwardIt1, typename Size, typename ForwardIt2, typename T, typename BinaryReductionOp, typename BinaryOp>
+T transform_reduce_n(
+    const ::execution::par_t &, ForwardIt1 first1, Size n, ForwardIt2 first2, T init, BinaryReductionOp reduce,
+    BinaryOp binary_op )
+{
+#pragma omp parallel
+    {
+        T local_result = zero_value<T>();
+#pragma omp for
+        for( Size i = 0; i < n; ++i )
+        {
+            local_result = reduce( local_result, binary_op( *( first1 + i ), *( first2 + i ) ) );
+        }
+#pragma critical
+        init = reduce( init, local_result );
     }
     return init;
 }
