fd1305fb44ca836d0c655f09f2ca4efb4d7d9e96
puerling <t.puerling@fz-juelich.de>
2025-07-23 13:43:08 +0200

    DOCS: add documentation for the `BiaxialAnisotropyData` class

--- core/python/spirit/hamiltonian.py
+++ core/python/spirit/hamiltonian.py
@@ -519,6 +519,17 @@ def get_biaxial_anisotropy_n_terms(p_state, idx_image=-1, idx_chain=-1):
 
 
 class BiaxialAnisotropyData(NamedTuple):
+    """
+    Data describing the Biaxial Anisotropy interaction
+
+    :param indices: indices
+    :param primary: primary anisotropy axis
+    :param secondary: secondary anisotropy axis
+    :param site_p: offset array for site data in magnitude and exponents
+    :param magnitude: magnitude of the contribution
+    :param exponents: exponents of the contribution
+    """
+
     indices: np.ndarray
     primary: np.ndarray
     secondary: np.ndarray
